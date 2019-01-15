07da65314ea6624b61d8427f1ab228357f58faca
Lex van der Werff <lex@Lexs-MacBook-Pro.local>
2019-01-15 21:19:02 +0100

    Added eight year

--- iterate.py
+++ iterate.py
@@ -1,11 +1,12 @@
 import sys
 from pymongo import MongoClient
 import json
+from datetime import datetime
 
 # INIT ALL MONGODB STUFF
 client = MongoClient('localhost', 27017)
 db = client.iterate
-col = db.utxo
+col = db.utxomore
 
 # CREATE LIST FOR ALL THE JSON
 rawutxolist = []
@@ -34,22 +35,61 @@ utxodata = []
 # DICT FOR COUNTED DATA
 singleblockdata = {
     "blockheight": firstutxo["blockheight"],
-    "blocktimestamp": firstutxo["blocktimestamp"],
+    "blocktimestamp": datetime.utcfromtimestamp(firstutxo["blocktimestamp"]).strftime('%Y-%m-%d %H:%M:%S'),
     "totalamount": 0,
     "totalutxos": 0,
-    "youngerthenday": 0,
-    "youngerthenweek": 0,
-    "youngerthenmonth": 0,
-    "youngerthenthreemonth": 0,
-    "youngerthensixmonth": 0,
-    "youngerthenyear": 0,
-    "youngerthenonehalfyear": 0,
-    "youngerthentwoyear": 0,
-    "youngerthenthreeyear": 0,
-    "youngerthenfouryear": 0,
-    "youngerthenfiveyear": 0,
-    "youngerthensixyear": 0,
-    "olderthensexyear": 0
+    "nuytday": 0,
+    "nuytweek": 0,
+    "nuytmonth": 0,
+    "nuytthreemonth": 0,
+    "nuytsixmonth": 0,
+    "nuytyear": 0,
+    "nuytonehalfyear": 0,
+    "nuyttwoyear": 0,
+    "nuytthreeyear": 0,
+    "nuytfouryear": 0,
+    "nuytsixyear": 0,
+    "nuyteightyear": 0,
+    "nuoteightyear": 0,
+    "puytday": 0,
+    "puytweek": 0,
+    "puytmonth": 0,
+    "puytthreemonth": 0,
+    "puytsixmonth": 0,
+    "puytyear": 0,
+    "puytonehalfyear": 0,
+    "puyttwoyear": 0,
+    "puytthreeyear": 0,
+    "puytfouryear": 0,
+    "puytsixyear": 0,
+    "puyteightyear": 0,
+    "puoteightyear": 0,
+    "ncytday": 0,
+    "ncytweek": 0,
+    "ncytmonth": 0,
+    "ncytthreemonth": 0,
+    "ncytsixmonth": 0,
+    "ncytyear": 0,
+    "ncytonehalfyear": 0,
+    "ncyttwoyear": 0,
+    "ncytthreeyear": 0,
+    "ncytfouryear": 0,
+    "ncytsixyear": 0,
+    "ncyteightyear": 0,
+    "ncoteightyear": 0,
+    "pcytday": 0,
+    "pcytweek": 0,
+    "pcytmonth": 0,
+    "pcytthreemonth": 0,
+    "pcytsixmonth": 0,
+    "pcytyear": 0,
+    "pcytonehalfyear": 0,
+    "pcyttwoyear": 0,
+    "pcytthreeyear": 0,
+    "pcytfouryear": 0,
+    "pcytsixyear": 0,
+    "pcyteightyear": 0,
+    "pcoteightyear": 0
 }
 
 # AGE LISTS SECONDS
@@ -63,36 +103,177 @@ onehalfyear = 47304036
 twoyear = 63072048
 threeyear = 94608072
 fouryear = 126144096
-fiveyear = 157680120
 sixyear = 189216144
+eightyear = 252288192
+
+print("JUST BEFORE PYTHON LOOP: ", datetime.now())
 
 # LOOP THROUGH
 for utxo in rawutxolist:
     if utxo["blockheight"] == previousheight:
-        singleblockdata["totalamount"] = singleblockdata["totalamount"] + (utxo["utxosatoshis"] / 1000000)
+        singleblockdata["totalamount"] = singleblockdata["totalamount"] + (utxo["utxosatoshis"] / 100000000)
         singleblockdata["totalutxos"] = singleblockdata["totalutxos"] + 1
         
         if utxo["utxoage"] <= day:
-            singleblockdata["youngerthenday"] = singleblockdata["youngerthenday"] + 1
+            singleblockdata["nuytday"] = singleblockdata["nuytday"] + 1
+            singleblockdata["ncytday"] = singleblockdata["ncytday"] + (utxo["utxosatoshis"] / 100000000)
         elif day < utxo["utxoage"] <= week:
-            singleblockdata["youngerthenweek"] = singleblockdata["youngerthenweek"] + 1
+            singleblockdata["nuytweek"] = singleblockdata["nuytweek"] + 1
+            singleblockdata["ncytweek"] = singleblockdata["ncytweek"] + (utxo["utxosatoshis"] / 100000000)
+        elif week < utxo["utxoage"] <= month:
+            singleblockdata["nuytmonth"] = singleblockdata["nuytmonth"] + 1
+            singleblockdata["ncytmonth"] = singleblockdata["ncytmonth"] + (utxo["utxosatoshis"] / 100000000)
+        elif month < utxo["utxoage"] <= threemonth:
+            singleblockdata["nuytthreemonth"] = singleblockdata["nuytthreemonth"] + 1
+            singleblockdata["ncytthreemonth"] = singleblockdata["ncytthreemonth"] + (utxo["utxosatoshis"] / 100000000)
+        elif threemonth < utxo["utxoage"] <= sixmonth:
+            singleblockdata["nuytsixmonth"] = singleblockdata["nuytsixmonth"] + 1
+            singleblockdata["ncytsixmonth"] = singleblockdata["ncytsixmonth"] + (utxo["utxosatoshis"] / 100000000)
+        elif sixmonth < utxo["utxoage"] <= year:
+            singleblockdata["nuytyear"] = singleblockdata["nuytyear"] + 1
+            singleblockdata["ncytyear"] = singleblockdata["ncytyear"] + (utxo["utxosatoshis"] / 100000000)
+        elif year < utxo["utxoage"] <= onehalfyear:
+            singleblockdata["nuytonehalfyear"] = singleblockdata["nuytonehalfyear"] + 1
+            singleblockdata["ncytonehalfyear"] = singleblockdata["ncytonehalfyear"] + (utxo["utxosatoshis"] / 100000000)
+        elif onehalfyear < utxo["utxoage"] <= twoyear:
+            singleblockdata["nuyttwoyear"] = singleblockdata["nuyttwoyear"] + 1
+            singleblockdata["ncyttwoyear"] = singleblockdata["ncyttwoyear"] + (utxo["utxosatoshis"] / 100000000)
+        elif twoyear < utxo["utxoage"] <= threeyear:
+            singleblockdata["nuytthreeyear"] = singleblockdata["nuytthreeyear"] + 1
+            singleblockdata["ncytthreeyear"] = singleblockdata["ncytthreeyear"] + (utxo["utxosatoshis"] / 100000000)
+        elif threeyear < utxo["utxoage"] <= fouryear:
+            singleblockdata["nuytfouryear"] = singleblockdata["nuytfouryear"] + 1
+            singleblockdata["ncytfouryear"] = singleblockdata["ncytfouryear"] + (utxo["utxosatoshis"] / 100000000)
+        elif fouryear < utxo["utxoage"] <= sixyear:
+            singleblockdata["nuytsixyear"] = singleblockdata["nuytsixyear"] + 1
+            singleblockdata["ncytsixyear"] = singleblockdata["ncytsixyear"] + (utxo["utxosatoshis"] / 100000000)
+        elif sixyear < utxo["utxoage"] <= eightyear:
+            singleblockdata["nuyteightyear"] = singleblockdata["nuyteightyear"] + 1
+            singleblockdata["ncyteightyear"] = singleblockdata["ncyteightyear"] + (utxo["utxosatoshis"] / 100000000)
+        elif eightyear < utxo["utxoage"]:
+            singleblockdata["nuoteightyear"] = singleblockdata["nuoteightyear"] + 1
+            singleblockdata["ncoteightyear"] = singleblockdata["ncoteightyear"] + (utxo["utxosatoshis"] / 100000000)
     else:
+        # SOME CALC FOR PERCENTAGE OF TOTAL
+
+        totalutxos = singleblockdata["totalutxos"] / 100
+        totalcoins = singleblockdata["totalamount"] / 100
+
+        singleblockdata["puytday"] = singleblockdata["nuytday"] / totalutxos
+        singleblockdata["puytweek"] = singleblockdata["nuytweek"] / totalutxos
+        singleblockdata["puytmonth"] = singleblockdata["nuytmonth"] / totalutxos
+        singleblockdata["puytthreemonth"] = singleblockdata["nuytthreemonth"] / totalutxos
+        singleblockdata["puytsixmonth"] = singleblockdata["nuytsixmonth"] / totalutxos
+        singleblockdata["puytyear"] = singleblockdata["nuytyear"] / totalutxos
+        singleblockdata["puytonehalfyear"] = singleblockdata["nuytonehalfyear"] / totalutxos
+        singleblockdata["puyttwoyear"] = singleblockdata["nuyttwoyear"] / totalutxos
+        singleblockdata["puytthreeyear"] = singleblockdata["nuytthreeyear"] / totalutxos
+        singleblockdata["puytfouryear"] = singleblockdata["nuytfouryear"] / totalutxos
+        singleblockdata["puytsixyear"] = singleblockdata["nuytsixyear"] / totalutxos
+        singleblockdata["puyteightyear"] = singleblockdata["nuyteightyear"] / totalutxos
+        singleblockdata["puoteightyear"] = singleblockdata["nuoteightyear"] / totalutxos
+
+        singleblockdata["pcytday"] = singleblockdata["ncytday"] / totalcoins
+        singleblockdata["pcytweek"] = singleblockdata["ncytweek"] / totalcoins
+        singleblockdata["pcytmonth"] = singleblockdata["ncytmonth"] / totalcoins
+        singleblockdata["pcytthreemonth"] = singleblockdata["ncytthreemonth"] / totalcoins
+        singleblockdata["pcytsixmonth"] = singleblockdata["ncytsixmonth"] / totalcoins
+        singleblockdata["pcytyear"] = singleblockdata["ncytyear"] / totalcoins
+        singleblockdata["pcytonehalfyear"] = singleblockdata["ncytonehalfyear"] / totalcoins
+        singleblockdata["pcyttwoyear"] = singleblockdata["ncyttwoyear"] / totalcoins
+        singleblockdata["pcytthreeyear"] = singleblockdata["ncytthreeyear"] / totalcoins
+        singleblockdata["pcytfouryear"] = singleblockdata["ncytfouryear"] / totalcoins
+        singleblockdata["pcytsixyear"] = singleblockdata["ncytsixyear"] / totalcoins
+        singleblockdata["pcyteightyear"] = singleblockdata["ncyteightyear"] / totalcoins
+        singleblockdata["pcoteightyear"] = singleblockdata["ncoteightyear"] / totalcoins
+
         utxodata.append(singleblockdata)
 
+        # RESET BLOCK DATA
         singleblockdata = {
             "blockheight": utxo["blockheight"],
-            "blocktimestamp": utxo["blocktimestamp"],
-            "totalamount": (utxo["utxosatoshis"] / 1000000),
-            "totalutxos": 1
+            "blocktimestamp": datetime.utcfromtimestamp(utxo["blocktimestamp"]).strftime('%Y-%m-%d %H:%M:%S'),
+            "totalamount": (utxo["utxosatoshis"] / 100000000),
+            "totalutxos": 1,
+            "nuytday": 0,
+            "nuytweek": 0,
+            "nuytmonth": 0,
+            "nuytthreemonth": 0,
+            "nuytsixmonth": 0,
+            "nuytyear": 0,
+            "nuytonehalfyear": 0,
+            "nuyttwoyear": 0,
+            "nuytthreeyear": 0,
+            "nuytfouryear": 0,
+            "nuytsixyear": 0,
+            "nuyteightyear": 0,
+            "nuoteightyear": 0,
+            "ncytday": 0,
+            "ncytweek": 0,
+            "ncytmonth": 0,
+            "ncytthreemonth": 0,
+            "ncytsixmonth": 0,
+            "ncytyear": 0,
+            "ncytonehalfyear": 0,
+            "ncyttwoyear": 0,
+            "ncytthreeyear": 0,
+            "ncytfouryear": 0,
+            "ncytsixyear": 0,
+            "ncyteightyear": 0,
+            "ncoteightyear": 0,
         }
+
+        if utxo["utxoage"] <= day:
+            singleblockdata["nuytday"] = singleblockdata["nuytday"] + 1
+            singleblockdata["ncytday"] = singleblockdata["ncytday"] + (utxo["utxosatoshis"] / 100000000)
+        elif day < utxo["utxoage"] <= week:
+            singleblockdata["nuytweek"] = singleblockdata["nuytweek"] + 1
+            singleblockdata["ncytweek"] = singleblockdata["ncytweek"] + (utxo["utxosatoshis"] / 100000000)
+        elif week < utxo["utxoage"] <= month:
+            singleblockdata["nuytmonth"] = singleblockdata["nuytmonth"] + 1
+            singleblockdata["ncytmonth"] = singleblockdata["ncytmonth"] + (utxo["utxosatoshis"] / 100000000)
+        elif month < utxo["utxoage"] <= threemonth:
+            singleblockdata["nuytthreemonth"] = singleblockdata["nuytthreemonth"] + 1
+            singleblockdata["ncytthreemonth"] = singleblockdata["ncytthreemonth"] + (utxo["utxosatoshis"] / 100000000)
+        elif threemonth < utxo["utxoage"] <= sixmonth:
+            singleblockdata["nuytsixmonth"] = singleblockdata["nuytsixmonth"] + 1
+            singleblockdata["ncytsixmonth"] = singleblockdata["ncytsixmonth"] + (utxo["utxosatoshis"] / 100000000)
+        elif sixmonth < utxo["utxoage"] <= year:
+            singleblockdata["nuytyear"] = singleblockdata["nuytyear"] + 1
+            singleblockdata["ncytyear"] = singleblockdata["ncytyear"] + (utxo["utxosatoshis"] / 100000000)
+        elif year < utxo["utxoage"] <= onehalfyear:
+            singleblockdata["nuytonehalfyear"] = singleblockdata["nuytonehalfyear"] + 1
+            singleblockdata["ncytonehalfyear"] = singleblockdata["ncytonehalfyear"] + (utxo["utxosatoshis"] / 100000000)
+        elif onehalfyear < utxo["utxoage"] <= twoyear:
+            singleblockdata["nuyttwoyear"] = singleblockdata["nuyttwoyear"] + 1
+            singleblockdata["ncyttwoyear"] = singleblockdata["ncyttwoyear"] + (utxo["utxosatoshis"] / 100000000)
+        elif twoyear < utxo["utxoage"] <= threeyear:
+            singleblockdata["nuytthreeyear"] = singleblockdata["nuytthreeyear"] + 1
+            singleblockdata["ncytthreeyear"] = singleblockdata["ncytthreeyear"] + (utxo["utxosatoshis"] / 100000000)
+        elif threeyear < utxo["utxoage"] <= fouryear:
+            singleblockdata["nuytfouryear"] = singleblockdata["nuytfouryear"] + 1
+            singleblockdata["ncytfouryear"] = singleblockdata["ncytfouryear"] + (utxo["utxosatoshis"] / 100000000)
+        elif fouryear < utxo["utxoage"] <= sixyear:
+            singleblockdata["nuytsixyear"] = singleblockdata["nuytsixyear"] + 1
+            singleblockdata["ncytsixyear"] = singleblockdata["ncytsixyear"] + (utxo["utxosatoshis"] / 100000000)
+        elif sixyear < utxo["utxoage"] <= eightyear:
+            singleblockdata["nuyteightyear"] = singleblockdata["nuyteightyear"] + 1
+            singleblockdata["ncyteightyear"] = singleblockdata["ncyteightyear"] + (utxo["utxosatoshis"] / 100000000)
+        elif eightyear < utxo["utxoage"]:
+            singleblockdata["nuoteightyear"] = singleblockdata["nuoteightyear"] + 1
+            singleblockdata["ncoteightyear"] = singleblockdata["ncoteightyear"] + (utxo["utxosatoshis"] / 100000000)
 
     previousheight = utxo["blockheight"]
     previoustimestamp = utxo["blocktimestamp"]
 
-print(utxodata)
+print("START DATA INSERT AT: ", datetime.now())
 
 # INSERT ALL THE DATA AT ONCE
-# col.insert_many(datalist)
+col.insert_many(utxodata)
+
+print("UTXO DATA INSERTED AT: ", datetime.now())
+
+print("LAST BLOCK HEIGHT: ", utxodata[-1]["blockheight"])
 
 # CLOSE PYMONGO CLIENT
 client.close()
